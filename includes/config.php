56982bc543985bf327c467d20100a23d6aef17b9
D4NTE <111151866+D4NTE98@users.noreply.github.com>
2025-02-21 23:28:40 +0100

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,12 +1,9 @@
 <?php
 session_start();
-$host = 'localhost';
-$db   = 'movies';
-$user = 'root';
-$pass = '';
-$charset = 'utf8mb4';
+$user = "root";
+$pass = "password";
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$dsn = "mysql:host=localhost;dbname=movies;charset=utf8mb4";
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
